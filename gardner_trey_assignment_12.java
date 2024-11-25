c510994e47fd24cd50844cf33e5ef72cfd61bfec
Trey Gardner <treygardner72@gmail.com>
2024-11-25 09:40:07 -0700

    Update gardner_trey_assignment_12.java

--- gardner_trey_assignment_12.java
+++ gardner_trey_assignment_12.java
@@ -1,7 +1,26 @@
 
 public class gardner_trey_assignment_12 {
 	public static void main(String[] args) {
-		 
+		
 		
 	}
+}
+
+class Animal{
+}
+
+class Bear extends Animal {
+
+}
+
+class Elephant extends Animal {
+
+}
+
+class Monkey extends Animal {
+
+}
+
+class Sloth extends Animal {
+	
 }
