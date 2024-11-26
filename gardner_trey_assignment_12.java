7f89361e993949586c047e3d28fdcde60c81c142
Trey Gardner <treygardner72@gmail.com>
2024-11-26 07:36:30 -0700

    Built Classes

--- gardner_trey_assignment_12.java
+++ gardner_trey_assignment_12.java
@@ -1,26 +1,45 @@
 
 public class gardner_trey_assignment_12 {
 	public static void main(String[] args) {
-		
+	
+	//length of array is pulled from "Animals.txt" file	
+	//polymorphic animal array
+	Animal[] animals = new Animal[];
 		
 	}
+
+//method that reads file and creates animal objects?
+
+//method that places animal objects into polymorphic arrray
 }
 
+//super class
 class Animal{
+
+	private String name;
+	private String food;
+	private double weight; 
+	private double timeSleep;
+	private String location;
+
 }
 
+//sub class (bear)
 class Bear extends Animal {
 
 }
 
+//sub class (elephant)
 class Elephant extends Animal {
 
 }
 
+//sub class (monkey)
 class Monkey extends Animal {
 
 }
 
+//sub class (sloth)
 class Sloth extends Animal {
 	
 }
